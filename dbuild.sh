bcc645c6fc32b2ad74c99af269521083bc57ed3c
方帅 <fangshuai02@meituan.com>
2020-10-13 17:01:49 +0800

    docker

--- dbuild.sh
+++ dbuild.sh
@@ -8,29 +8,29 @@ if [[ "$version" == "" ]]; then
 fi
 
 
-mvn clean install
+mvn clean install --settings /Users/fangshuai/.m2/settings.aliyun.xml -Dmaven.test.skip=true
 
 
-echo "exec : docker build . -t fuhai/jpress:"${version}
-docker build . -t fuhai/jpress:${version}
+echo "exec : docker build . -t fangs2046/jpress:"${version}
+docker build . -t fangs2046/jpress:${version}
 
 
-echo "exec : docker push fuhai/jpress:"${version}
-docker push fuhai/jpress:${version}
+echo "exec : docker push fangs2046/jpress:"${version}
+docker push fangs2046/jpress:${version}
 
 
-echo "exec : docker push fuhai/jpress:latest"
-docker tag fuhai/jpress:${version} fuhai/jpress:latest
-docker push fuhai/jpress:latest
+echo "exec : docker push fangs2046/jpress:latest"
+docker tag fangs2046/jpress:${version} fangs2046/jpress:latest
+docker push fangs2046/jpress:latest
 
 
-echo "exec : docker push jpressio/jpress:"${version}
-docker tag fuhai/jpress:latest jpressio/jpress:${version}
-docker push jpressio/jpress:${version}
-
-
-echo "exec : docker push jpressio/jpress:latest"
-docker tag jpressio/jpress:${version} jpressio/jpress:latest
-docker push jpressio/jpress:latest
+#echo "exec : docker push jpressio/jpress:"${version}
+#docker tag fangs2046/jpress:latest jpressio/jpress:${version}
+#docker push jpressio/jpress:${version}
+#
+#
+#echo "exec : docker push jpressio/jpress:latest"
+#docker tag jpressio/jpress:${version} jpressio/jpress:latest
+#docker push jpressio/jpress:latest
 
 
